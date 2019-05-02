423f1e239e4cf68cdd008c5e8acf5af3d74118a1
Farshad Danaee Fard <fdanaeefard@gmail.com>
2019-05-02 22:48:07 +0430

    UI practice

--- app/src/main/java/com/farshadfard/helloworldsematec/MainActivity.java
+++ app/src/main/java/com/farshadfard/helloworldsematec/MainActivity.java
@@ -3,6 +3,7 @@ package com.farshadfard.helloworldsematec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("activityLog", "onCreate: Called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("activityLog", "onStart: Called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("activityLog", "onResume: Called");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("activityLog", "onPause: Called");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("activityLog", "onStop: Called");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("activityLog", "onDestroy: Called");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d("activityLog", "onRestart: Called");
+
     }
 }
